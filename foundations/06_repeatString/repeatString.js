f7eebb228d360fe475e8024433100008a985acf1
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-06 20:20:41 -0700

    Complete Exercise 06 - repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let arr = [];
+    if (num >= 1) {
+        for (i = 0; i < num; i++) {
+            arr.push(str);
+        }
+    } else if (num === 0) {
+        arr.pop();
+    } else {
+        return 'ERROR';
+    } 
+    return arr.join('');
 };
 
 // Do not edit below this line
