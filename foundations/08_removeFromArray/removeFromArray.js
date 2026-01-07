cf51a1c727df8876babe1d74c3fc6acb189cbce3
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-06 21:48:27 -0700

    Complete Exercise 08 - removeFromArray.js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let newArr = [... new Set(arr)];
+    console.log(newArr);
+    for (const n of num) {
+        if (newArr.includes(n)) {
+            ind = newArr.indexOf(n);
+            newArr.splice(ind, 1);
+        }
+    } 
+    return newArr;
 };
+
+removeFromArray([1, 2, 3, 4], 7, 'tacos');
 
 // Do not edit below this line
 module.exports = removeFromArray;
