e1541257b37a64a56c6139e8359ecc46e74bf9ac
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-08 11:15:34 -0700

    Complete Exercise 09 - sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -3,15 +3,31 @@ const sumAll = function(start, end) {
     // make a variable to add all values between start and end
     let sum = 0;
 
-    // get each value between start and end and add them together
-    for (let i = start; i <= end; i++) {
-        console.log(i);
-        sum += i;
+    // check if both arguments are integers
+    if (Number.isInteger(start) && Number.isInteger(end)) {
+        // check if arguments are non-negative numbers
+        if (start > 0 && end > 0) {
+            // check if larger number is first or second argument
+            if (start < end) {
+                // get each value between start and end and add them together
+                for (let i = start; i <= end; i++) {
+                    sum += i;
+                }
+            } else {
+                for (let i = end; i <= start; i++) {
+                    sum += i;
+                }
+            }
+        } else {
+            return 'ERROR';
+        }
+    } else {
+        return 'ERROR';
     }
     return sum;
 };
 
-sumAll(2, 4);
+sumAll(2.5, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
