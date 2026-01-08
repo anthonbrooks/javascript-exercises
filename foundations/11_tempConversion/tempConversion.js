ded7f0ed0d49009cd748089b52dff08a6edd94e9
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-08 13:21:33 -0700

    Pass second Farenheit test - Exercise 11 - tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -2,7 +2,8 @@ const convertToCelsius = function(temp) {
 };
 
 const convertToFahrenheit = function(temp) {
-  return temp * (9/5) + 32;
+  let faren = temp * (9/5) + 32;
+  return +faren.toFixed(1);
 };
 
 // Do not edit below this line
