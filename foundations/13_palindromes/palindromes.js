373aaf0e5e369ac75542a8192a91032128321b25
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-09 15:30:36 -0700

    Complete palindromes() function for Exercise 13 - Palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    revStr = str.split('').reverse().join('');
+    if (str === revStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
