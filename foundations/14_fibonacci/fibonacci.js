1ec4fdd8b42f06d73f95e40d02a1910a24d4803b
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-09 17:13:44 -0700

    Fibonacci function complete/test 1 passed Exercise 14 Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+// Fibonacci Sequence (formula F(n) = F(n - 1) + F(n-2))
+const fibonacci = function(nthMember) {
+    // fibSeq = [0, 1, 1, 2, 3, 5, 8];
+    // return fibSeq[nthMember];
 
+    let fib = [1, 1];
+
+    for (let i = 2; i < nthMember; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    return fib[nthMember - 1];
 };
 
 // Do not edit below this line
