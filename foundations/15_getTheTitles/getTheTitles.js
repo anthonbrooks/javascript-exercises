c43164ce6f641999fd33e079e1b90222fdbebf45
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-09 18:12:56 -0700

    Complete getTheTitles function - Exercise 15 - Get the Titles clear

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookList) {
 
+    let bookTitles = [];
+
+    // loop through array to get each book
+    for (let book in bookList) {
+
+        bookTitles.push(bookList[book].title);
+    }
+
+    return bookTitles;
 };
 
 // Do not edit below this line
